924e2fc9c74dfb0a8f2f643ac27f6c9198d6a064
Purvit Patel <purvitpatel@Purvits-Laptop.cable.rcn.com>
2024-02-25 16:05:27 -0600

    All buttons work.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let opdFlag = 0; //0 indicates that the operand is taking value and it goes to 1 the moment any operand is clicked.
 let optFlag = '';
 let opd1, opd2;
-const ans = document.querySelector('#ans');
 
+const ans = document.querySelector('#ans');
 const opd = document.querySelectorAll('.operand');
 const opt = document.querySelectorAll('.operator');
 const equalTo = document.querySelector('#bEqualto');
+const sign = document.querySelector('#signChange');
+const clear = document.querySelector('#bClear');
+const percent = document.querySelector('#percent');
 
 opd.forEach(element => {
     element.addEventListener('click', (event) => {
@@ -41,19 +44,48 @@ opt.forEach(element => {
 });
 
 equalTo.addEventListener('click', () => {
+    
     opd2 = parseFloat(ans.innerHTML);
     if(optFlag === '+'){
         ans.innerHTML = opd1 + opd2;
     }
-    if(optFlag === '-'){
+    else if(optFlag === '-'){
         ans.innerHTML = opd1 - opd2;
     }
-    if(optFlag === '*'){
+    else if(optFlag === '*'){
         ans.innerHTML = opd1 * opd2;
     }
-    if(optFlag === '/'){
+    else if(optFlag === '/'){
         ans.innerHTML = opd1 / opd2;
     }
+    else if(optFlag === '%'){
+        ans.innerHTML = opd1 % opd2;
+    }
 
+})
+
+sign.addEventListener('click', () => {
+    let screen = parseFloat(ans.innerHTML);
+    if(screen > 0){
+        ans.innerHTML = '-' + ans.innerHTML;
+    }
+    else{
+        ans.innerHTML = ans.innerHTML.slice(1);
+    }
+    
+
+})
+
+clear.addEventListener('click', () => {
+    
+    ans.innerHTML = '';
+    opdFlag = 0;
+    optFlag = '';
+    opd1, opd2 = 0,0;
+})
+
+percent.addEventListener('click', () => {
+    
+    ans.innerHTML = parseFloat(ans.innerHTML)/ 100;
 })
 
